b65645de24103a3bb5dae844e7f082228e4f6d67
shankkarpal46 <shankkarpal46@gmail.com>
2024-12-19 11:50:04 +0530

    function and its types

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+function hello(name){
+    document.write("<h1>"+name+"</h1>")
+            }
+
+//non parameterized
+// function hello(){
+//     document.write("<h1>Hello</h1>")
+//             }
+
+
+function add(a,b){
+    return a+b;
+}
+
+hello("Arti")
+hello("Shankar")
+hello("Trupti")
+
+alert(add(12,78)+100)
